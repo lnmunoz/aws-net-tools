29950741e125f1d2ef54fc11ef1bee6f17f316e5
Luis Nazareno MUÑOZ <luisnazareno.munoz@naranja.com>
2022-08-20 02:22:50 -0300

    Fixed versions

--- curl.sh
+++ curl.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
-ex = yum list installed | grep curl
-if [ -z "$ex"]
-then 
-    echo "No está instalado "
+#if [ -z "yum list installed | grep curl"]
+#Check linux distro
+cat /etc/os-release | grep ubuntu > ubuntu.txt 
+cat /etc/os-release | grep amazon > amazon.txt 
+ubuntu=ubuntu.txt
+amazon=amazon.txt
+if [ -s "$ubuntu" ];then 
+    echo "Tu distribución es Ubuntu"
+    dpkg -l | grep curl > pkg.txt
+    file=pkg.txt
+elif [ -s "$amazon" ];then 
+    echo "Tu distribución es Amazon"
+    yum list installed | grep curl > pkg.txt
+    file=pkg.txt
 else
-    echo "Está instalado"
+    echo "Distribución no compatible"
 fi
+
+
+
+if [ -s "$file" ];then 
+    echo "Curl está instalado "
+else
+    echo "Curl no está instalado"
+
+fi
+rm ubuntu.txt
+rm amazon.txt
+rm pkg.txt
 #yum update  
 #yum install curl -y
 echo "----------------------------------------------------"
