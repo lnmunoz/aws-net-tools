bb5a9a5a95fde6702823ba799514fbc63367295c
Luis Nazareno MUÑOZ <luisnazareno.munoz@naranja.com>
2022-08-20 01:47:08 -0300

    Add curl and some checks

--- curl.sh
+++ curl.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+ex = yum list installed | grep curl
+if [ -z "$ex"]
+then 
+    echo "No está instalado "
+else
+    echo "Está instalado"
+fi
 #yum update  
 #yum install curl -y
 echo "----------------------------------------------------"
